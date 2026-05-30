a4bf3169a7d8fb87fc1b0df247a779c3056ec31e
GforsZi <givaldigumelarsetiawan@gmail.com>
2026-05-30 22:21:41 +0700

    fix bug

--- app/modules/users/schemas.py
+++ app/modules/users/schemas.py
@@ -9,3 +9,10 @@ class UserUpdate(BaseModel):
     username: str | None = None
     email: EmailStr | None = None
 
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    
+    class Config:
+        from_attributes = True
